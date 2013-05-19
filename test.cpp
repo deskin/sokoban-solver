66eb1d0b045e9db753a2d411e1124d20f9791bca
Deskin Miller <deskin@outlook.com>
2013-05-19 06:01:26 +0000

    Add simple test file
    Need to factor into makefile.

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE basic test
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(tautology_test) {
+	BOOST_CHECK_EQUAL(0, 0);
+	BOOST_CHECK_EQUAL(1, 0);
+}

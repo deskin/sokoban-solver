57f73b54acd921653af6c4c3cb1504438c95fba9
Deskin Miller <deskin@outlook.com>
2013-05-20 13:34:32 +0000

    Add failing level::parse test

--- test/level.cpp
+++ test/level.cpp
@@ -2,12 +2,19 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
+#include <string>
 #include "level.h"
 
 BOOST_AUTO_TEST_SUITE(level)
 
 BOOST_AUTO_TEST_CASE(construct) {
 	sokoban::level();
+}
+
+BOOST_AUTO_TEST_CASE(parse) {
+	sokoban::level level;
+	std::string s("@`^");
+	BOOST_CHECK_NO_THROW(level.parse(s));
 }
 
 BOOST_AUTO_TEST_SUITE_END()

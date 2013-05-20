6a12b2a11ef5778c133beec7a013c308dd839d4b
Deskin Miller <deskin@outlook.com>
2013-05-20 14:01:16 +0000

    Add failing level::parse_too_many_men

--- test/level.cpp
+++ test/level.cpp
@@ -3,6 +3,7 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 #include <string>
+#include "errors.h"
 #include "level.h"
 
 BOOST_AUTO_TEST_SUITE(level)
@@ -17,4 +18,9 @@ BOOST_AUTO_TEST_CASE(parse) {
 	BOOST_CHECK_NO_THROW(level.parse(s));
 }
 
+BOOST_AUTO_TEST_CASE(parse_too_many_men) {
+	sokoban::level level;
+	std::string s("@@");
+	BOOST_CHECK_THROW(level.parse(s), sokoban::level_parse_exception);
+}
 BOOST_AUTO_TEST_SUITE_END()

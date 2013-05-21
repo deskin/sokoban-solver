8a2a51c90ad95383255f6cd15bcbb2e9ca15eaf7
Deskin Miller <deskin@outlook.com>
2013-05-21 02:39:12 +0000

    Make level::parse_not_enough_rocks pass

--- level.cpp
+++ level.cpp
@@ -8,14 +8,24 @@ void
 level::parse(const std::string &s)
 {
 	size_t avatar_count = 0;
+	size_t rock_count = 0;
+	size_t pit_count = 0;
 
 	for (const char &c : s) {
 		if (c == '@') {
 			++avatar_count;
+		} else if (c == '`') {
+			++rock_count;
+		} else if (c == '^') {
+			++pit_count;
 		}
 	}
 
 	if (avatar_count != 1) {
+		throw level_parse_exception();
+	}
+
+	if (rock_count < pit_count) {
 		throw level_parse_exception();
 	}
 }

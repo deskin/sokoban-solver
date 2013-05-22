65bf23eef6e50f5989cfb248b243dc4d3a40f4b5
Deskin Miller <deskin@outlook.com>
2013-05-22 02:04:12 +0000

    Make level::parse_get_avatar_2_1 pass
    Tdd-save

--- level.cpp
+++ level.cpp
@@ -8,16 +8,31 @@ void
 level::parse(const std::string &s)
 {
 	size_t avatar_count = 0;
-	size_t rock_count = 0;
 	size_t pit_count = 0;
+	size_t rock_count = 0;
+	size_t column = 0;
+	size_t row = 0;
 
 	for (const char &c : s) {
 		if (c == '@') {
+			if (avatar_count != 0) {
+				throw level_parse_exception();
+			}
+
 			++avatar_count;
+			avatar_position.first = row;
+			avatar_position.second = column;
 		} else if (c == '`') {
 			++rock_count;
 		} else if (c == '^') {
 			++pit_count;
+		}
+
+		if (c == '\n') {
+			++column;
+			row = 0;
+		} else {
+			++row;
 		}
 	}
 

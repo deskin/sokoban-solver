b7f6bbeda2b23495b02e94adb82749ae9bb4cbb9
Deskin Miller <deskin@outlook.com>
2013-06-01 22:17:24 +0000

    Add level::ostream_insertion

--- test/level.cpp
+++ test/level.cpp
@@ -5,6 +5,7 @@
 
 #include <algorithm>
 #include <set>
+#include <sstream>
 #include <string>
 #include "tuple-hack.h"
 #include <utility>
@@ -219,6 +220,17 @@ BOOST_AUTO_TEST_CASE(parse_get_tile_avatar) {
 	BOOST_REQUIRE_NO_THROW(level.parse(s));
 	const sokoban::level::tiles_type &tiles(level.tiles());
 	BOOST_CHECK(tiles[1][5].avatar());
+}
+
+BOOST_AUTO_TEST_CASE(ostream_insertion) {
+	sokoban::level level;
+	std::string s(
+		"^.6.`.\n"
+		"   ..@\n");
+	BOOST_REQUIRE_NO_THROW(level.parse(s));
+	std::ostringstream os;
+	os << level;
+	BOOST_CHECK_EQUAL(s, os.str());
 }
 
 BOOST_AUTO_TEST_SUITE_END()

71b1b4b3aba0c1ce3b8a20f21ad6424d6e011fa3
Deskin Miller <deskin@outlook.com>
2013-06-03 16:19:41 +0000

    Make move::can_move_basic pass

---
+++ include/move.h
@@ -0,0 +1,30 @@
+#ifndef SOKOBAN_MOVE_H_
+#define SOKOBAN_MOVE_H_
+
+#include <type_traits>
+
+#include "level.h"
+
+namespace sokoban {
+
+namespace direction {
+
+enum class value {
+	up,
+	right,
+	down,
+	left
+};
+
+typedef std::integral_constant<value, value::up> up;
+typedef std::integral_constant<value, value::right> right;
+typedef std::integral_constant<value, value::down> down;
+typedef std::integral_constant<value, value::left> left;
+
+} // namespace direction
+
+bool can_move(const level &l, direction::value d);
+
+} // namespace sokoban
+
+#endif // SOKOBAN_MOVE_H_
---
+++ move.cpp
@@ -0,0 +1,12 @@
+#include "level.h"
+#include "move.h"
+
+namespace sokoban {
+
+bool
+can_move(const level &l, direction::value d)
+{
+	return true;
+}
+
+} // namespace sokoban

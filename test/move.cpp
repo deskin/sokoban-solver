13abe37689645e8c6fc1df0a8cde2cfda4dec64b
Deskin Miller <deskin@outlook.com>
2013-06-11 18:57:48 +0000

    Add passing move::can_move_rock

--- test/move.cpp
+++ test/move.cpp
@@ -21,6 +21,21 @@ BOOST_AUTO_TEST_CASE(can_move_basic) {
 	BOOST_CHECK(sokoban::can_move(level, sokoban::direction::left()));
 }
 
+BOOST_AUTO_TEST_CASE(can_move_rock) {
+	sokoban::level level;
+	std::string s(
+		".....\n"
+		"^.`..\n"
+		".`@`.\n"
+		"..`..\n"
+		".....\n");
+	BOOST_REQUIRE_NO_THROW(level.parse(s));
+	BOOST_CHECK(sokoban::can_move(level, sokoban::direction::up()));
+	BOOST_CHECK(sokoban::can_move(level, sokoban::direction::right()));
+	BOOST_CHECK(sokoban::can_move(level, sokoban::direction::down()));
+	BOOST_CHECK(sokoban::can_move(level, sokoban::direction::left()));
+}
+
 BOOST_AUTO_TEST_CASE(cannot_move_wall) {
 	sokoban::level level;
 	sokoban::level level2;

9b204d6a0a695b4ece3d86f08291858ab0654b2d
Deskin Miller <deskin@outlook.com>
2013-06-15 22:08:54 +0000

    Add move::get_level_rock

--- test/move.cpp
+++ test/move.cpp
@@ -153,22 +153,59 @@ BOOST_AUTO_TEST_CASE(get_level) {
 		sokoban::move(level, sokoban::direction::left()));
 }
 
-#if 0
-BOOST_AUTO_TEST_CASE(get_level) {
+BOOST_AUTO_TEST_CASE(get_level_rock) {
 	sokoban::level level;
-	sokoban::level level2;
+	sokoban::level level_up;
+	sokoban::level level_right;
+	sokoban::level level_down;
+	sokoban::level level_left;
 	std::string s(
-		"^`.\n"
-		"@..\n");
-	std::string s2(
-		"7`.\n"
-		"...\n");
+		"  ^  \n"
+		"  `  \n"
+		".`@`.\n"
+		"  `  \n"
+		"  .  \n");
+	std::string s_up(
+		"  6  \n"
+		"  @  \n"
+		".`.`.\n"
+		"  `  \n"
+		"  .  \n");
+	std::string s_right(
+		"  ^  \n"
+		"  `  \n"
+		".`.@`\n"
+		"  `  \n"
+		"  .  \n");
+	std::string s_down(
+		"  ^  \n"
+		"  `  \n"
+		".`.`.\n"
+		"  @  \n"
+		"  `  \n");
+	std::string s_left(
+		"  ^  \n"
+		"  `  \n"
+		"`@.`.\n"
+		"  `  \n"
+		"  .  \n");
 	BOOST_REQUIRE_NO_THROW(level.parse(s));
-	BOOST_REQUIRE_NO_THROW(level2.parse(s2));
+	BOOST_REQUIRE_NO_THROW(level_up.parse(s_up));
+	BOOST_REQUIRE_NO_THROW(level_right.parse(s_right));
+	BOOST_REQUIRE_NO_THROW(level_down.parse(s_down));
+	BOOST_REQUIRE_NO_THROW(level_left.parse(s_left));
 	BOOST_CHECK_EQUAL(
-		level2,
+		level_up,
 		sokoban::move(level, sokoban::direction::up()));
+	BOOST_CHECK_EQUAL(
+		level_right,
+		sokoban::move(level, sokoban::direction::right()));
+	BOOST_CHECK_EQUAL(
+		level_down,
+		sokoban::move(level, sokoban::direction::down()));
+	BOOST_CHECK_EQUAL(
+		level_left,
+		sokoban::move(level, sokoban::direction::left()));
 }
-#endif
 
 BOOST_AUTO_TEST_SUITE_END()

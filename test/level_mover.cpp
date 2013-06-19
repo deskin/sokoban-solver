4e2356283093131c7f804ceefacd0ef3f2035d58
Deskin Miller <deskin@outlook.com>
2013-06-19 02:30:58 +0000

    Add level_mover::size_one

---
+++ test/level_mover.cpp
@@ -0,0 +1,18 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <string>
+
+#include "level.h"
+
+BOOST_AUTO_TEST_SUITE(level_mover)
+
+BOOST_AUTO_TEST_CASE(size_one) {
+	sokoban::level level;
+	std::string s("^`@\n");
+	BOOST_REQUIRE_NO_THROW(level.parse(s));
+	sokoban::level_mover mover(level);
+	BOOST_CHECK_EQUAL(1, mover.size());
+}
+
+BOOST_AUTO_TEST_SUITE_END()

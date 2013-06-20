cf30adecd3c49ec1663f49bbbb6fb35e08d7ba77
Deskin Miller <deskin@outlook.com>
2013-06-20 00:52:54 +0000

    Extend level_mover::size

--- test/level_mover.cpp
+++ test/level_mover.cpp
@@ -8,12 +8,17 @@
 
 BOOST_AUTO_TEST_SUITE(level_mover)
 
-BOOST_AUTO_TEST_CASE(size_one) {
+BOOST_AUTO_TEST_CASE(size) {
 	sokoban::level level;
+	sokoban::level level2;
 	std::string s("^`@\n");
+	std::string s2(
+		"^`@\n"
+		" ..\n");
 	BOOST_REQUIRE_NO_THROW(level.parse(s));
-	sokoban::level_mover mover(level);
-	BOOST_CHECK_EQUAL(1, mover.size());
+	BOOST_REQUIRE_NO_THROW(level2.parse(s2));
+	BOOST_CHECK_EQUAL(1, sokoban::level_mover(level).size());
+	BOOST_CHECK_EQUAL(2, sokoban::level_mover(level2).size());
 }
 
 BOOST_AUTO_TEST_SUITE_END()

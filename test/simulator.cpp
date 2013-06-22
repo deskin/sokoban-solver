1e0fdc1fdce60e9bb55b1cc65601ed8ebda3ba57
Deskin Miller <deskin@outlook.com>
2013-06-22 23:18:21 +0000

    Modify simulator::construct to construct from string

--- test/simulator.cpp
+++ test/simulator.cpp
@@ -3,16 +3,13 @@
 
 #include <string>
 
-#include "level.h"
 #include "simulator.h"
 
 BOOST_AUTO_TEST_SUITE(simulator)
 
 BOOST_AUTO_TEST_CASE(construct) {
-	sokoban::level level;
 	std::string s("6@.\n");
-	BOOST_REQUIRE_NO_THROW(level.parse(s));
-	BOOST_REQUIRE_NO_THROW(sokoban::simulator sim(level));
+	BOOST_REQUIRE_NO_THROW(sokoban::simulator sim(s));
 }
 
 BOOST_AUTO_TEST_SUITE_END()

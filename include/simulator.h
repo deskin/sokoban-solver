f5cd53770cab2668bc0104f9cd95ea1a9a39ce39
Deskin Miller <deskin@outlook.com>
2013-06-22 23:16:44 +0000

    Make simulator::costruct pass

---
+++ include/simulator.h
@@ -0,0 +1,15 @@
+#ifndef SOKOBAN_SIMULATOR_H_
+#define SOKOBAN_SIMULATOR_H_
+
+#include "level.h"
+
+namespace sokoban {
+
+class simulator {
+public:
+	simulator(const level &) {}
+};
+
+} // namespace sokoban
+
+#endif // SOKOBAN_SIMULATOR_H_

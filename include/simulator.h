9b52a0fcd1791361789957721680ff89efb7aa3c
Deskin Miller <deskin@outlook.com>
2013-06-22 23:43:50 +0000

    Make simuator::get_steps pass

--- include/simulator.h
+++ include/simulator.h
@@ -2,12 +2,25 @@
 #define SOKOBAN_SIMULATOR_H_
 
 #include <string>
+#include <vector>
+
+#include "level.h"
 
 namespace sokoban {
 
 class simulator {
 public:
-	explicit simulator(const std::string &) {}
+	typedef std::vector<level> steps_type;
+
+private:
+	steps_type level_steps;
+
+public:
+	explicit simulator(const std::string &) :
+		level_steps()
+	{}
+
+	const steps_type &steps() const { return level_steps; }
 };
 
 } // namespace sokoban

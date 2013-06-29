4be2979b30f4b3bff6f6d0d6b5e054ec924320aa
Deskin Miller <deskin@outlook.com>
2013-06-29 23:17:27 +0000

    WIP get max steps
    Need to write tests for this.

--- simulator.cpp
+++ simulator.cpp
@@ -6,6 +6,38 @@
 #include "level.h"
 #include "simulator.h"
 
+namespace detail {
+
+size_t
+count_level_arrangements(const sokoban::level &l)
+{
+	size_t open_tiles = 0;
+	for (const auto &row: l.tiles()) {
+		for (const auto &tile: row) {
+			if (tile.is_valid()) {
+				++open_tiles;
+			}
+		}
+	}
+
+	size_t rock_count = l.rocks().size();
+	size_t tile_product = open_tiles - rock_count;
+	if (rock_count > tile_product) {
+		rock_count = tile_product;
+	}
+
+	size_t rock_product = 1;
+
+	for (; rock_count > 0; --open_tiles, --rock_count) {
+		tile_product *= open_tiles;
+		rock_product *= rock_count;
+	}
+
+	return tile_product / rock_product;
+}
+
+} // namespace detail
+
 namespace sokoban {
 
 simulator::simulator(const std::string &s) :
@@ -29,6 +61,9 @@ void
 simulator::run()
 {
 	has_run = true;
+
+	size_t max_steps = detail::count_level_arrangements(level_steps[0]);
+	if (max_steps) {}
 }
 
 } // namespace sokoban
---
+++ test/simulator-test.h
@@ -0,0 +1,12 @@
+#ifndef SOKOBAN_SIMULATOR_TEST_H_
+#define SOKOBAN_SIMULATOR_TEST_H_
+
+#include "level.h"
+
+namespace detail {
+
+size_t count_level_arrangements(const sokoban::level &l);
+
+} // namespace detail
+
+#endif // SOKOBAN_SIMULATOR_TEST_H_

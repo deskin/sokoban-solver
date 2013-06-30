8c77a2b5402073a6e2c6983c54353a233229b2a0
Deskin Miller <deskin@outlook.com>
2013-06-30 05:03:14 +0000

    Basic main function

--- sokoban.cpp
+++ sokoban.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <string>
+#include <utility>
+
+#include "errors.h"
+#include "simulator.h"
+
+namespace sok = sokoban;
 
 int main(int argc, char **argv) {
-	std::wcout << L"Hello world!" << std::endl;
+	std::string s;
+	std::string line;
+
+	while(std::getline(std::cin, line)) {
+		s += std::move(line);
+		s += "\n";
+	}
+
+	try {
+		sok::simulator sim(s);
+		sim.run();
+
+		if (sim.win()) {
+			const sok::simulator::steps_type &steps(
+				sim.steps());
+			std::cout << "Solved in "
+				<< steps.size() - 1
+				<< " moves."
+				<< std::endl;
+		}
+	} catch (const sok::level_parse_exception &) {
+		std::cerr << "Level parsing error!" << std::endl;
+	}
 }

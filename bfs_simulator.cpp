205f9d000e060f7e4415f98ab33679402d1ba349
Deskin Miller <deskin@outlook.com>
2013-07-04 23:16:57 +0000

    Make bfs_simulator::get_steps pass

---
+++ bfs_simulator.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+#include "bfs_simulator.h"
+
+namespace sokoban {
+
+bfs_simulator::bfs_simulator(const std::string &) :
+	level_steps()
+{}
+
+} // namespace sokoban
--- include/bfs_simulator.h
+++ include/bfs_simulator.h
@@ -2,12 +2,22 @@
 #define SOKOBAN_BFS_SIMULATOR_H_
 
 #include <string>
+#include <vector>
+
+#include "level.h"
 
 namespace sokoban {
 
 class bfs_simulator {
 public:
-	explicit bfs_simulator(const std::string &) {}
+	typedef std::vector<level> steps_type;
+
+	explicit bfs_simulator(const std::string &);
+
+	const steps_type &steps() const { return level_steps; }
+
+private:
+	steps_type level_steps;
 };
 
 } // namespace sokoban

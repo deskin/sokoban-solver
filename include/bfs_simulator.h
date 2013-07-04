6c3934a1f440c2d52f164097d2faf62d42fc4247
Deskin Miller <deskin@outlook.com>
2013-07-04 22:44:42 +0000

    Make bfs_simulator:construct pass

---
+++ include/bfs_simulator.h
@@ -0,0 +1,15 @@
+#ifndef SOKOBAN_BFS_SIMULATOR_H_
+#define SOKOBAN_BFS_SIMULATOR_H_
+
+#include <string>
+
+namespace sokoban {
+
+class bfs_simulator {
+public:
+	explicit bfs_simulator(const std::string &) {}
+};
+
+} // namespace sokoban
+
+#endif // SOKOBAN_BFS_SIMULATOR_H_

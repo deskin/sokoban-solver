49c1af790f6eee07f6d9c26d43e351ac21021344
Deskin Miller <deskin@outlook.com>
2013-07-04 23:04:18 +0000

    Add bfs_simulator::get_steps

--- test/bfs_simulator.cpp
+++ test/bfs_simulator.cpp
@@ -12,4 +12,10 @@ BOOST_AUTO_TEST_CASE(construct) {
 	BOOST_CHECK_NO_THROW(sokoban::bfs_simulator sim(s));
 }
 
+BOOST_AUTO_TEST_CASE(get_steps) {
+	std::string s("6@.\n");
+	sokoban::bfs_simulator sim(s);
+	BOOST_CHECK_NO_THROW(sim.steps());
+}
+
 BOOST_AUTO_TEST_SUITE_END()

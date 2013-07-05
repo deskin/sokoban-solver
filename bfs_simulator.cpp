d63256685b38c82308a6e00c31a37f17492b6b85
Deskin Miller <deskin@outlook.com>
2013-07-05 05:13:12 +0000

    WIP

--- bfs_simulator.cpp
+++ bfs_simulator.cpp
@@ -1,4 +1,7 @@
+#include <map>
+#include <set>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "analyze.h"
@@ -28,5 +31,21 @@ void
 bfs_simulator::run()
 {
 	has_run = true;
+	map_type evens;
+	std::set<level> initial_set;
+	map_type odds;
+	std::set<level> odd_set;
+
+	evens.insert(std::make_pair(level_steps[0], level_pointer()));
+	initial_set.insert(level_steps[0]);
 }
+bool
+bfs_simulator::solve(bfs_simulator::map_type &current,
+	bfs_simulator::map_type &next,
+	bfs_simulator::set_type &current_set,
+	bfs_simulator::set_type &next_set)
+{
+
+}
+
 } // namespace sokoban
--- include/bfs_simulator.h
+++ include/bfs_simulator.h
@@ -1,6 +1,8 @@
 #ifndef SOKOBAN_BFS_SIMULATOR_H_
 #define SOKOBAN_BFS_SIMULATOR_H_
 
+#include <map>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -8,7 +10,38 @@
 
 namespace sokoban {
 
+namespace detail {
+
+class level_pointer;
+
+typedef std::map<level, level_pointer> bfs_map_type;
+
+class level_pointer {
+public:
+	typedef bfs_map_type::iterator value_type;
+
+	level_pointer() :
+		i(),
+		is_empty(true)
+	{}
+
+	explicit level_pointer(value_type i) :
+		i(i),
+		is_empty(false)
+	{}
+
+private:
+	value_type i;
+	bool is_empty;
+};
+
+} // namespace detail
+
 class bfs_simulator {
+private:
+	typedef detail::bfs_map_type map_type;
+	typedef std::set<level> set_type;
+
 public:
 	typedef std::vector<level> steps_type;
 
@@ -23,6 +56,11 @@ public:
 private:
 	steps_type level_steps;
 	bool has_run;
+
+	bool solve(map_type &current,
+		map_type &next,
+		set_type &current_set,
+		set_type &next_set);
 };
 
 } // namespace sokoban
